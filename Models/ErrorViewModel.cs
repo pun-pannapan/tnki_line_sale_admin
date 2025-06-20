67609c4bd39fdf007c506635b00461b46fccd12f
pun-pannapan <pannapantu@gmail.com>
2025-06-20 17:24:58 +0700

    initial admin site

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tnki_line_sale_admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
